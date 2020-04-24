5e953b49fb1b60fce55a6b8284c886f6a60891dd
blakelower <blakelower1999@gmail.com>
2020-04-24 15:27:24 -0700

    added test

--- api/server.spec.js
+++ api/server.spec.js
@@ -1,33 +1,49 @@
 const request = require("supertest");
 
-const server = require('./server');
+const server = require("./server");
 
-describe("server.js", function(){
-    describe("checking for env var", function() {
-        it('set env to testing', function() {
-            expect(process.env.DB_ENV).toBe("test")
-        })
-    })
-})
+describe("server.js", function () {
+  describe("checking for env var", function () {
+    it("set env to testing", function () {
+      expect(process.env.DB_ENV).toBe("test");
+    });
+  });
+});
 
-describe("GET /", function() {
-    it("should recieve a 200 success message", function(){
+describe("GET /", function () {
+  it("should recieve a 200 success message", function () {
+    return request(server)
+      .get("/")
+      .then((res) => {
+        expect(res.status).toBe(200);
+      })
+      it("i usually get an error", () => {
         return request(server)
-        .get('/')
+        .get("/login")
         .then(res => {
-            expect(res.status).toBe(200);
-        })
-    })
-    describe("GET /api/users", () => {
-        it('should return: shall not pass!', async () => {
-            const res = await request(server).get("/api/users");
-            expect(res.body.you).toBe(undefined);
-        })
-    })
-    describe ("POST /api/auth/login", () => {
-        it("should tell me im logged in", async () =>{
-          const res=await request(server).get("/api/auth/login");
-          expect(200)
+          expect(res.status).toBe(404)
         })
       })
-})
+  });
+
+  describe("GET /api/users", () => {
+    it("should return: shall not pass!", async () => {
+      const res = await request(server).get("/api/users");
+      expect(res.body.you).toBe(undefined);
+    });
+  });
+
+  describe("POST /api/auth/login", () => {
+    it("should tell me im logged in", async () => {
+      const res = await request(server).get("/api/auth/login");
+      expect(200);
+    });
+    it("should recieve error", async () => {
+      await request(server)
+        .post("/register")
+        .then((res) => {
+          expect(res.status).toBe(404);
+        });
+    });
+  });
+});
